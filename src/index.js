e976c299a1cf556cbd544c39910bb099e26bc8af
Sergey Volkov <serzhvolkov03@gmail.com>
2023-01-06 17:22:26 +0300

    feat: implement function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let result = "";
+    const numberModul = Math.abs(n);
+    for (number of numberModul.toString()) {
+        result = number + `${result}`;
+    }
+    return Number(result);
+};
